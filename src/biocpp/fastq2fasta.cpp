aad73cdd047b52a1e77c23c3eece49fac105894b
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-07-12 15:29:02 +0100

    conversion from fastq to fasta

--- src/biocpp/fastq2fasta.cpp
+++ src/biocpp/fastq2fasta.cpp
@@ -1,4 +1,13 @@
+#include <string_view>
+#include <seqan3/io/sequence_file/all.hpp>
 
-int main()
+// Alias for a c-style string.
+using character_string = char const *;
+
+int main(int argc, character_string argv[])
 {
+	std::string_view fastq_file_path{argv[1]};
+
+	seqan3::sequence_file_output{std::cout, seqan3::format_fasta{}} =
+		seqan3::sequence_file_input{fastq_file_path};
 }
